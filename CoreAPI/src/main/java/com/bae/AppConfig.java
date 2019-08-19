9ef2692b34d9c23ee801179198309b174a6439d2
Monika-Mistry <monika.mistry1@gmail.com>
2019-08-19 14:51:52 +0100

    audit queue config

---
+++ CoreAPI/src/main/java/com/bae/AppConfig.java
@@ -0,0 +1,18 @@
+package com.bae;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jms.support.converter.MappingJackson2MessageConverter;
+import org.springframework.jms.support.converter.MessageConverter;
+import org.springframework.jms.support.converter.MessageType;
+
+@Configuration
+public class AppConfig {
+	@Bean
+	public MessageConverter jacksonJmsMessageConverter() {
+		MappingJackson2MessageConverter converter = new MappingJackson2MessageConverter();
+		converter.setTargetType(MessageType.TEXT);
+		converter.setTypeIdPropertyName("_type");
+		return converter;
+	}
+
+}
