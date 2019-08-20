ab9412c971a38cc4af6d3ad5f9af03fabca4ca5c
Monika-Mistry <monika.mistry1@gmail.com>
2019-08-20 17:04:26 +0100

    setup

---
+++ AuditConsumer/src/main/java/com/bae/AuditConsumerApplication.java
@@ -0,0 +1,13 @@
+package com.bae;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuditConsumerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuditConsumerApplication.class, args);
+	}
+
+}
