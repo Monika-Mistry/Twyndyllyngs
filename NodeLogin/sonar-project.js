ae1f41b09dfd007e0722038c7013141e195f944d
Monika-Mistry <monika.mistry1@gmail.com>
2019-08-20 13:19:21 +0000

    sonarqube fixes

--- NodeLogin/sonar-project.js
+++ NodeLogin/sonar-project.js
@@ -3,6 +3,6 @@ const sonarqubeScanner = require('sonarqube-scanner');
        serverUrl: 'http://23.251.140.243:9000',
        options : {
        'sonar.sources': '.',
-       'sonar.inclusions' : 'src/**' // Entry point of your code
+       //'sonar.inclusions' : '.' // Entry point of your code
        }
      }, () => {});
