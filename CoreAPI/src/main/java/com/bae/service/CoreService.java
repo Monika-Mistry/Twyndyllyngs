54ee549c8c848c2bd32510f68ac1d85e04b3459a
Monika-Mistry <monika.mistry1@gmail.com>
2019-08-20 10:39:05 +0100

    removed service packages

--- CoreAPI/src/main/java/com/bae/service/CoreService.java
+++
@@ -1,6 +0,0 @@
-package com.bae.service;
-
-public interface CoreService {
-	
-	public boolean doesUserExist();
-}
--- CoreAPI/src/main/java/com/bae/service/CoreServiceImplementation.java
+++
@@ -1,9 +0,0 @@
-package com.bae.service;
-
-public class CoreServiceImplementation implements CoreService{
-
-	public boolean doesUserExist() {
-		return false;
-	}
-
-}
