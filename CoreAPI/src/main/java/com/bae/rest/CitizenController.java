84777bd30f79518b13e0830f590104ac2b3dd4fc
Monika-Mistry <monika.mistry1@gmail.com>
2019-08-20 09:57:12 +0100

    testing for citizen controller with citizen[]

--- CoreAPI/src/main/java/com/bae/rest/CitizenController.java
+++ CoreAPI/src/main/java/com/bae/rest/CitizenController.java
@@ -1,7 +1,6 @@
 package com.bae.rest;
 
 import java.sql.Timestamp;
-import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.ParameterizedTypeReference;
@@ -28,51 +27,48 @@ public class CitizenController extends Controller {
 	}
 
 	@GetMapping("getCitizensByForenames/{username}/{forenames}")
-	public ResponseEntity<List<Citizen>> findCitizenByForenames(@PathVariable("forenames") String forenames,
+	public ResponseEntity<Citizen[]> findCitizenByForenames(@PathVariable("forenames") String forenames,
 			@PathVariable("username") String username) {
 
-		Request request = new Request(username, forenames, new Timestamp(System.currentTimeMillis()));
-
-		sendToQueue(request);
+//		Request request = new Request(username, forenames, new Timestamp(System.currentTimeMillis()));
+//
+//		sendToQueue(request);
 
 		return restTemplate.exchange(Constants.CITIZEN_URL + "/getCitizensByForenames/" + username + "/" + forenames,
-				HttpMethod.GET, null, new ParameterizedTypeReference<List<Citizen>>() {
-				});
+				HttpMethod.GET, null, Citizen[].class);
 
 	}
 
 	@GetMapping("/getCitizensBySurname/{username}/{surname}")
-	public ResponseEntity<List<Citizen>> findCitizenBySurname(@PathVariable("surname") String surname,
+	public ResponseEntity<Citizen[]> findCitizenBySurname(@PathVariable("surname") String surname,
 			@PathVariable("username") String username) {
-		Request request = new Request(username, surname, new Timestamp(System.currentTimeMillis()));
+//		Request request = new Request(username, surname, new Timestamp(System.currentTimeMillis()));
+//
+//		sendToQueue(request);
 
-		sendToQueue(request);
-
-		return restTemplate.exchange(Constants.CITIZEN_URL + "/getCitizensByForenames/" + username + "/" + surname,
-				HttpMethod.GET, null, new ParameterizedTypeReference<List<Citizen>>() {
-				});
+		return restTemplate.exchange(Constants.CITIZEN_URL + "/getCitizensBySurname/" + username + "/" + surname,
+				HttpMethod.GET, null, Citizen[].class);
 	}
 
 	@GetMapping("/getCitizensByFullname/{username}/{forenames}/{surname}")
-	public ResponseEntity<List<Citizen>> findCitizenByForenamesAndSurname(@PathVariable("forenames") String forenames,@PathVariable("surname") String surname,
+	public ResponseEntity<Citizen[]> findCitizenByForenamesAndSurname(@PathVariable("forenames") String forenames,@PathVariable("surname") String surname,
 			@PathVariable("username") String username) {
-		Request request = new Request(username, forenames + " " + surname, new Timestamp(System.currentTimeMillis()));
+//		Request request = new Request(username, forenames + " " + surname, new Timestamp(System.currentTimeMillis()));
+//
+//		sendToQueue(request);
 
-		sendToQueue(request);
-
-		return restTemplate.exchange(Constants.CITIZEN_URL + "/getCitizensByForenames/" + username + "/" + forenames + "/" + surname,
-				HttpMethod.GET, null, new ParameterizedTypeReference<List<Citizen>>() {
-				});
+		return restTemplate.exchange(Constants.CITIZEN_URL + "/getCitizensByFullname/" + username + "/" + forenames + "/" + surname,
+				HttpMethod.GET, null, Citizen[].class);
 	}
 
 	@GetMapping("/getCitizenById/{username}/{citizenId}")
 	public Citizen findCitizenById(@PathVariable("id") Long id,
 			@PathVariable("username") String username) {
-		Request request = new Request(username,"Citizen ID:" +  id, new Timestamp(System.currentTimeMillis()));
+//		Request request = new Request(username,"Citizen ID:" +  id, new Timestamp(System.currentTimeMillis()));
+//
+//		sendToQueue(request);
 
-		sendToQueue(request);
-
-		return restTemplate.getForObject(Constants.CITIZEN_URL + "/getCitizensByForenames/" + username + "/" + id, Citizen.class);
+		return restTemplate.getForObject(Constants.CITIZEN_URL + "/getCitizenById/" + username + "/" + id, Citizen.class);
 	}
 
 }
--- CoreAPI/src/test/java/com/bae/rest/CitizenControllerTest.java
+++ CoreAPI/src/test/java/com/bae/rest/CitizenControllerTest.java
@@ -1,5 +1,7 @@
 package com.bae.rest;
 
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.doReturn;
 import static org.mockito.Mockito.when;
 
 import java.util.ArrayList;
@@ -7,12 +9,14 @@ import java.util.List;
 
 import javax.net.ssl.SSLEngineResult.Status;
 
+import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.web.client.RestTemplate;
@@ -23,22 +27,65 @@ import com.bae.domain.Citizen;
 
 @RunWith(SpringRunner.class)
 public class CitizenControllerTest {
-	
+
 	@InjectMocks
 	private CitizenController controller;
-	
+
 	@Mock
 	private RestTemplate restTemplate;
-	
+
 	@Test
 	public void findByForenameTest() {
 		List<Citizen> MOCK_LIST = new ArrayList<>();
 		MOCK_LIST.add(TestConstants.MOCK_CITIZEN);
-		ResponseEntity<List<Citizen>> response = new ResponseEntity<List<Citizen>>(MOCK_LIST, null, Status.OK);
-		
-		when(restTemplate.exchange(Constants.CITIZEN_URL + "/getCitizensByForenames/1/Crazy",
-				HttpMethod.GET, null, new ParameterizedTypeReference<List<Citizen>>() {
-				})).thenReturn(response)
+
+		Citizen[] citizens = MOCK_LIST.toArray(new Citizen[MOCK_LIST.size()]);
+
+		ResponseEntity<Citizen[]> response = new ResponseEntity<Citizen[]>(citizens, HttpStatus.OK);
+
+		doReturn(response).when(restTemplate).exchange(Constants.CITIZEN_URL + "/getCitizensByForenames/user/Crazy",
+				HttpMethod.GET, null, Citizen[].class);
+
+		assertEquals(response, controller.findCitizenByForenames("Crazy", "user"));
+	}
+
+	@Test
+	public void findByIdTest() {
+
+		doReturn(TestConstants.MOCK_CITIZEN).when(restTemplate).getForObject(Constants.CITIZEN_URL + "/getCitizenById/user/" + 12348L,
+				Citizen.class);
+
+		assertEquals(TestConstants.MOCK_CITIZEN, controller.findCitizenById(12348L, "user"));
+	}
+
+	@Test
+	public void findBySurnameTest() {
+		List<Citizen> MOCK_LIST = new ArrayList<>();
+		MOCK_LIST.add(TestConstants.MOCK_CITIZEN);
+
+		Citizen[] citizens = MOCK_LIST.toArray(new Citizen[MOCK_LIST.size()]);
+
+		ResponseEntity<Citizen[]> response = new ResponseEntity<Citizen[]>(citizens, HttpStatus.OK);
+
+		doReturn(response).when(restTemplate).exchange(Constants.CITIZEN_URL + "/getCitizensBySurname/user/Ivan",
+				HttpMethod.GET, null, Citizen[].class);
+
+		assertEquals(response, controller.findCitizenBySurname("Ivan", "user"));
+	}
+
+	@Test
+	public void findByForenameAndSurnameTest() {
+		List<Citizen> MOCK_LIST = new ArrayList<>();
+		MOCK_LIST.add(TestConstants.MOCK_CITIZEN);
+
+		Citizen[] citizens = MOCK_LIST.toArray(new Citizen[MOCK_LIST.size()]);
+
+		ResponseEntity<Citizen[]> response = new ResponseEntity<Citizen[]>(citizens, HttpStatus.OK);
+
+		doReturn(response).when(restTemplate).exchange(Constants.CITIZEN_URL + "/getCitizensByFullname/user/Crazy/Ivan",
+				HttpMethod.GET, null, Citizen[].class);
+
+		assertEquals(response, controller.findCitizenByForenamesAndSurname("Crazy", "Ivan", "user"));
 	}
 
 }
