2fcb151b2592da09e39bb44f89439cde94b04dda
Monika-Mistry <monika.mistry1@gmail.com>
2019-08-20 14:59:36 +0100

    removed nodelogin - failed QA

--- NodeLogin/sonar-project.js
+++
@@ -1,8 +0,0 @@
-const sonarqubeScanner = require('sonarqube-scanner');
-     sonarqubeScanner({
-       serverUrl: 'http://23.251.140.243:9000',
-       options : {
-       'sonar.sources': '.',
-       //'sonar.inclusions' : '.' // Entry point of your code
-       }
-     }, () => {});
