a816fa4c20cad7102cdc2df0246cb0e82b32782d
Monika-Mistry <monika.mistry1@gmail.com>
2019-08-20 15:54:31 +0100

    refactoring

--- CoreAPI/src/main/java/com/bae/rest/UserController.java
+++ CoreAPI/src/main/java/com/bae/rest/UserController.java
@@ -1,6 +1,10 @@
 package com.bae.rest;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -8,20 +12,31 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
+import com.bae.Constants;
 import com.bae.domain.User;
+import com.bae.service.CoreService;
 
 @RestController
 public class UserController {
 
 	private RestTemplate restTemplate;
+	
+	private CoreService service;
 
 	public UserController(@Autowired RestTemplate restTemplate) {
 
 		this.restTemplate = restTemplate;
 	}
 	
-	@PostMapping("/login")
-	public void loginUser(@RequestBody User user) {
+	@PostMapping("/loginUser")
+	public ResponseEntity<String> loginUser(@RequestBody User user) {
+		if(service.verifyLogin(user)) {
+			HttpEntity<User> requestEntity = new HttpEntity<>(user);
+			return restTemplate.exchange(Constants.LOGIN_USER, HttpMethod.POST, requestEntity, String.class);
+		}
+		else {
+			return new ResponseEntity<String>("Invalid login credentials", HttpStatus.PRECONDITION_FAILED);
+		}
 		
 	}
 	
--- CoreAPI/src/test/java/com/bae/rest/CitizenControllerTest.java
+++ CoreAPI/src/test/java/com/bae/rest/CitizenControllerTest.java
@@ -2,19 +2,15 @@ package com.bae.rest;
 
 import static org.junit.Assert.assertEquals;
 import static org.mockito.Mockito.doReturn;
-import static org.mockito.Mockito.when;
 
 import java.util.ArrayList;
 import java.util.List;
 
-import javax.net.ssl.SSLEngineResult.Status;
-
-import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.springframework.core.ParameterizedTypeReference;
+
 import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -22,7 +18,6 @@ import org.springframework.jms.core.JmsTemplate;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.web.client.RestTemplate;
 
-import com.bae.Constants;
 import com.bae.TestConstants;
 import com.bae.domain.Citizen;
 
@@ -34,7 +29,7 @@ public class CitizenControllerTest {
 
 	@Mock
 	private RestTemplate restTemplate;
-	
+
 	@Mock
 	private JmsTemplate jmsTemplate;
 
@@ -47,8 +42,8 @@ public class CitizenControllerTest {
 
 		ResponseEntity<Citizen[]> response = new ResponseEntity<Citizen[]>(citizens, HttpStatus.OK);
 
-		doReturn(response).when(restTemplate).exchange(TestConstants.CITIZEN_FORENAME,
-				HttpMethod.GET, null, Citizen[].class);
+		doReturn(response).when(restTemplate).exchange(TestConstants.CITIZEN_FORENAME, HttpMethod.GET, null,
+				Citizen[].class);
 
 		assertEquals(response, controller.findCitizenByForenames("Crazy", "user"));
 	}
@@ -56,8 +51,7 @@ public class CitizenControllerTest {
 	@Test
 	public void findByIdTest() {
 
-		doReturn(TestConstants.MOCK_CITIZEN).when(restTemplate).getForObject(TestConstants.CITIZEN_ID,
-				Citizen.class);
+		doReturn(TestConstants.MOCK_CITIZEN).when(restTemplate).getForObject(TestConstants.CITIZEN_ID, Citizen.class);
 
 		assertEquals(TestConstants.MOCK_CITIZEN, controller.findCitizenById(12348L, "user"));
 	}
@@ -71,8 +65,8 @@ public class CitizenControllerTest {
 
 		ResponseEntity<Citizen[]> response = new ResponseEntity<Citizen[]>(citizens, HttpStatus.OK);
 
-		doReturn(response).when(restTemplate).exchange(TestConstants.CITIZEN_SURNAME,
-				HttpMethod.GET, null, Citizen[].class);
+		doReturn(response).when(restTemplate).exchange(TestConstants.CITIZEN_SURNAME, HttpMethod.GET, null,
+				Citizen[].class);
 
 		assertEquals(response, controller.findCitizenBySurname("Ivan", "user"));
 	}
@@ -86,8 +80,8 @@ public class CitizenControllerTest {
 
 		ResponseEntity<Citizen[]> response = new ResponseEntity<Citizen[]>(citizens, HttpStatus.OK);
 
-		doReturn(response).when(restTemplate).exchange(TestConstants.CITIZEN_FULLNAME,
-				HttpMethod.GET, null, Citizen[].class);
+		doReturn(response).when(restTemplate).exchange(TestConstants.CITIZEN_FULLNAME, HttpMethod.GET, null,
+				Citizen[].class);
 
 		assertEquals(response, controller.findCitizenByForenamesAndSurname("Crazy", "Ivan", "user"));
 	}
