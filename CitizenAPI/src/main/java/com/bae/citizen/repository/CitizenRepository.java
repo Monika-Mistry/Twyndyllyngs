cc4334d5e0934f890c02118e0d99e8dbc637efb7
Monika-Mistry <monika.mistry1@gmail.com>
2019-08-21 13:04:37 +0100

    @repository added

--- CitizenAPI/src/main/java/com/bae/citizen/repository/CitizenRepository.java
+++ CitizenAPI/src/main/java/com/bae/citizen/repository/CitizenRepository.java
@@ -3,9 +3,11 @@ package com.bae.citizen.repository;
 import java.util.List;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
 import com.bae.citizen.domain.Citizen;
 
+@Repository
 public interface CitizenRepository extends CrudRepository<Citizen, Long> {
 
 	public List<Citizen> findByForenames(String forenames);
