579c2752eb227fabd6524dad4df55e17eb656969
Monika-Mistry <monika.mistry1@gmail.com>
2019-08-21 13:08:31 +0100

    constants url updated

--- CoreAPI/src/main/java/com/bae/Constants.java
+++ CoreAPI/src/main/java/com/bae/Constants.java
@@ -3,15 +3,15 @@ package com.bae;
 public class Constants {
 	
 	public static final String CITIZEN_URL = "http://citizen:8003/";
-	public static final String CITIZEN_FORENAME_URL = "http://citizen:8003/getCitizensByForenames/";
-	public static final String CITIZEN_SURNAME_URL = "http://citizen:8003/getCitizensBySurname/";
-	public static final String CITIZEN_FULLNAME_URL = "http://citizen:8003/getCitizensByFullname/";
-	public static final String CITIZEN_ID_URL = "http://citizen:8003/getCitizenById/";
+	public static final String CITIZEN_FORENAME_URL = CITIZEN_URL + "getCitizensByForenames/";
+	public static final String CITIZEN_SURNAME_URL =  CITIZEN_URL + "getCitizensBySurname/";
+	public static final String CITIZEN_FULLNAME_URL =  CITIZEN_URL + "getCitizensByFullname/";
+	public static final String CITIZEN_ID_URL =  CITIZEN_URL + "getCitizenById/";
 	
 	
 	public static final String LOGIN_URL = "http://login:8002/";
-	public static final String LOGIN_USER_URL = "http://login:8002/loginUser";
-	public static final String LOGIN_CREATE_URL = "http://login:8002/createUser";
-	public static final String LOGIN_DELETE_URL = "http://login:8002/deleteUser/";
+	public static final String LOGIN_USER_URL =  LOGIN_URL + "loginUser";
+	public static final String LOGIN_CREATE_URL =  LOGIN_URL + "createUser";
+	public static final String LOGIN_DELETE_URL =  LOGIN_URL + "deleteUser/";
 
 }
