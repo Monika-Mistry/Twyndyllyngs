065afa1a556385f44af828024582671a45f72f59
jjbrooks251 <jjbrooks251@outlook.com>
2019-08-21 10:41:54 +0100

    change login button links

--- frontend/src/Components/subcomponents/Login.js
+++ frontend/src/Components/subcomponents/Login.js
@@ -3,20 +3,16 @@ import { Link } from 'react-router-dom';
 
 export class Login extends Component {
 
-    user = () => {
-        window.location = './home'
-    }
-
-    audit =() => {
-        window.location = './audit'
-    }
-
     render() {
         return (
             <div>
                 <p>Login Page</p>
-                <button onClick={this.user}> Analyst </button>
-                <button onClick={this.audit}> Audit </button>
+                <Link to='./home'>
+                    <button> Analyst </button>
+                </Link>
+                <Link to='./audit'>
+                    <button> Audit </button>
+                </Link>
                 <p> <Link to='/forgot'> Forgotten Password?</Link></p>
             </div>
         )
