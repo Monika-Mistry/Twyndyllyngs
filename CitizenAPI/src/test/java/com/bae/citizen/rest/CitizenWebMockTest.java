e38299065a31480c27306133adb50467a9bba8da
biomiller <35339306+biomiller@users.noreply.github.com>
2019-08-21 14:53:54 +0100

    Update CitizenWebMockTest.java

--- CitizenAPI/src/test/java/com/bae/citizen/rest/CitizenWebMockTest.java
+++ CitizenAPI/src/test/java/com/bae/citizen/rest/CitizenWebMockTest.java
@@ -12,6 +12,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
