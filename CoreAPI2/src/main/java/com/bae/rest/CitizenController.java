3fce0a14a1f931f13ff86dced669e70a329eec3a
Monika-Mistry <monika.mistry1@gmail.com>
2019-08-22 18:59:06 +0100

    Get citizen all methods

---
+++ CoreAPI2/src/main/java/com/bae/rest/CitizenController.java
@@ -0,0 +1,75 @@
+package com.bae.rest;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.jms.core.JmsTemplate;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import com.bae.domain.Citizen;
+import com.bae.domain.VehicleLocation;
+
+@RestController
+public class CitizenController {
+
+	private RestTemplate restTemplate;
+	private JmsTemplate jmsTemplate;
+
+	public CitizenController(JmsTemplate jmsTemplate, @Autowired RestTemplate restTemplate) {
+		super(jmsTemplate);
+		this.restTemplate = restTemplate;
+	}
+
+	@GetMapping("/{forename}/{surname}")
+	public ResponseEntity<Citizen> getAllCitizens(@PathVariable("forename") String forename,
+			@PathVariable("surname") String surname) {
+
+		if (forename == "null") {
+			ResponseEntity<Citizen> citizenSurname = getCitizenBySurname(surname);
+			Citizen citizen = citizenSurname.getBody();
+			return new ResponseEntity<Citizen>(citizen, HttpStatus.OK);
+		} else if (surname == "null") {
+			ResponseEntity<Citizen> citizenForename = getCitizenByForename(forename);
+			Citizen citizen = citizenForename.getBody();
+			return new ResponseEntity<Citizen>(citizen, HttpStatus.OK);
+
+		} else {
+			ResponseEntity<Citizen> citizenFullname = getCitizenByFullname(forename, surname);
+			Citizen citizen = citizenFullname.getBody();
+			return new ResponseEntity<Citizen>(citizen, HttpStatus.OK);
+		}
+
+	}
+
+	@GetMapping("/getCitizensByForenames/{forenames}")
+	private ResponseEntity<Citizen> getCitizenByForename(
+			@PathVariable("forenames") String forenames) {
+		return restTemplate.getForEntity(Constants.CITIZEN_FORENAME_URL, Citizen.class);
+
+	}
+	
+	@GetMapping("/getCitizensBySurname/{surname}")
+	private ResponseEntity<Citizen> getCitizenBySurname(
+			@PathVariable("surname") String surname) {
+		return restTemplate.getForEntity(Constants.CITIZEN_SURNAME_URL, Citizen.class);
+
+	}
+	
+	@GetMapping("/getCitizensByFullname/{forenames}/{surname}")
+	private ResponseEntity<Citizen> getCitizenByFullname(
+			@PathVariable("forenames") String forenames, @PathVariable("surname") String surname) {
+		return restTemplate.getForEntity(Constants.CITIZEN_FULLNAME_URL, Citizen.class);
+
+	}
+	
+	@GetMapping("/getCitizenById/{citizenId}")
+	private ResponseEntity<Citizen> getCitizenById(
+			@PathVariable("citizenId") String citizenId) {
+		return restTemplate.getForEntity(Constants.CITIZEN_ID_URL, Citizen.class);
+
+	}
+
+}
--- CoreAPI2/src/main/java/com/bae/rest/Constants.java
+++ CoreAPI2/src/main/java/com/bae/rest/Constants.java
@@ -5,4 +5,8 @@ public class Constants {
 	public static final String VEHICLE_LOCATION_URL = "http://vehicle:8004/getLocationsByRegistrationNo/";
 	public static final String VEHICLE_REGISTRATION_URL = "http://vehicle:8004/getVehiclesByRegistrationNo/";
 
+	public static final String CITIZEN_FORENAME_URL = "http://citizen:8003/getCitizensByForenames/";
+	public static final String CITIZEN_SURNAME_URL = "http://citizen:8003/getCitizensBySurname/";
+	public static final String CITIZEN_FULLNAME_URL = "http://citizen:8003/getCitizensByFullname/";
+	public static final String CITIZEN_ID_URL = "http://citizen:8003/getCitizenById/";
 }
