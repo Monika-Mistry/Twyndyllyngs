aa1dc6e334a81145622e140f908db0cc66e7f5c4
Monika-Mistry <monika.mistry1@gmail.com>
2019-08-22 12:45:28 +0100

    core website

--- frontend/src/Components/subcomponents/Constants/Constants.js
+++ frontend/src/Components/subcomponents/Constants/Constants.js
@@ -1,5 +1,4 @@
-export const coreApi = "http://34.90.138.185";
+export const coreApi = "http://core:8000";
 
 export const ACCESS_TOKEN = 'accessToken';
 
-// http://34.90.138.185/getCitizensBySurname/user/Smith
