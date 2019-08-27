6a48dd365befd9a40caefe582e5a7d887932715d
Monika-Mistry <Monika-Mistry@users.noreply.github.com>
2019-08-27 12:55:30 +0100

    Update CoreApi2ApplicationTests.java

--- CoreAPI/src/test/java/com/bae/CoreApi2ApplicationTests.java
+++ CoreAPI/src/test/java/com/bae/CoreApi2ApplicationTests.java
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class CoreApi2ApplicationTests {
+public class CoreApiApplicationTests {
 
 	@Test
 	public void contextLoads() {
