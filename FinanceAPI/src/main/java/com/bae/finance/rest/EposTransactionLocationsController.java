c556b041c329174aacfd39355022285a523204b4
Monika-Mistry <Monika-Mistry@users.noreply.github.com>
2019-08-27 16:12:52 +0100

    Update EposTransactionLocationsController.java

--- FinanceAPI/src/main/java/com/bae/finance/rest/EposTransactionLocationsController.java
+++ FinanceAPI/src/main/java/com/bae/finance/rest/EposTransactionLocationsController.java
@@ -26,13 +26,13 @@ public class EposTransactionLocationsController {
 	@GetMapping("/getEposTransactionsByFullnameAndAddress/{forenames}/{surname}/{address}")
 	public ResponseEntity<EposTransactionLocations[]> findEposTransactionsByFullnameAndAddress(@PathVariable("forenames") String forenames, @PathVariable("surname") String surname, @PathVariable("address") String address){
 		
-		return new ResponseEntity<EposTransactionLocations[]>(eposService.findByForenamesAndSurnameAndAddress(forenames, surname, address), HttpStatus.OK);
+		return new ResponseEntity<>(eposService.findByForenamesAndSurnameAndAddress(forenames, surname, address), HttpStatus.OK);
 	}
  	
 	@GetMapping("/getEposTransactionsByCardNumber/{cardNumber}")
 	public ResponseEntity<EposTransactionLocations[]> findEposTransactionsByCardNumber(@PathVariable("cardNumber") String cardNumber){
 		
-		return new ResponseEntity<EposTransactionLocations[]>(eposService.findByCardNumber(cardNumber), HttpStatus.OK);
+		return new ResponseEntity<>(eposService.findByCardNumber(cardNumber), HttpStatus.OK);
 	}
 	
 	
