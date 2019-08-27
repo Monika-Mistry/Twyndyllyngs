9ee4fdc7d0da5afaab0564d1af3c6e0d2e583354
Monika-Mistry <Monika-Mistry@users.noreply.github.com>
2019-08-27 09:22:42 +0100

    removed @override

--- AssociateAPI/src/main/java/com/bae/associate/service/AssociateServiceImpl.java
+++ AssociateAPI/src/main/java/com/bae/associate/service/AssociateServiceImpl.java
@@ -23,12 +23,10 @@ public class AssociateServiceImpl implements AssociateService {
 	}
 
 
-	@Override
 	public ArrayList<Associate> findAssociatesByPhonenumber(String phoneNumber) {
 		return associateRepository.findAssociatesByPhonenumber(phoneNumber);
 	}
 
-	@Override
 	public ArrayList<Associate> findAssociatesByFullnameAndAddress(String forenames, String surname, String address) {
 		return associateRepository.findAssociatesByFullnameAndAddress(forenames, surname, address);
 	}
