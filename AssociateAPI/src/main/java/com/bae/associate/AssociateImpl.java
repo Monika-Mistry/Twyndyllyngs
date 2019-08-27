ef332b8c19e2e13c4a40a1afddec6a29ba1afc1c
Monika-Mistry <Monika-Mistry@users.noreply.github.com>
2019-08-27 09:11:35 +0100

    removed comments and @override

--- AssociateAPI/src/main/java/com/bae/associate/AssociateImpl.java
+++ AssociateAPI/src/main/java/com/bae/associate/AssociateImpl.java
@@ -13,36 +13,33 @@ public class AssociateImpl implements Associate {
 		this.address = address;
 	}
 
-	@Override
+
 	public String getForenames() {
-		// TODO Auto-generated method stub
 		return null;
 	}
 
-	@Override
+
 	public String getSurname() {
-		// TODO Auto-generated method stub
+
 		return null;
 	}
 
-	@Override
+
 	public String getAddress() {
-		// TODO Auto-generated method stub
+
 		return null;
 	}
 
-	@Override
 	public void setForenames(String forenames) {
 		this.forenames = forenames;		
 	}
 
 
-	@Override
 	public void setSurname(String surname) {
 		this.surname = surname;		
 	}
 
-	@Override
+
 	public void setAddress(String address) {
 		this.address = address;		
 	}
