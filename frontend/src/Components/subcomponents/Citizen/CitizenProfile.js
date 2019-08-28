97b2dbcf3647c94136706dc497d7b85a8c8fb049
jjbrooks251 <jjbrooks251@outlook.com>
2019-08-28 18:04:30 +0100

    blank value arrays

--- frontend/src/Components/subcomponents/Citizen/CitizenProfile.js
+++ frontend/src/Components/subcomponents/Citizen/CitizenProfile.js
@@ -13,13 +13,12 @@ export class CitizenProfile extends Component {
             forenames: null,
             surname: null,
             address: null,
-            associates: ["hello", "bye bye", "aardvark"],
+            associates: [],
             citizen: { citizenId: "", forenames: "", surname: "", dob: "", gender: "", pob: "", address: "" },
             vehicle: [],
             mobile:[],
             callRecords:[],
-            finances: [],
-            associates2: []
+            finances: []
         }
     }
 
@@ -45,10 +44,10 @@ export class CitizenProfile extends Component {
 
             this.setState({
                 citizen: response.data[0],
-                callRecords: [{ id: 1, timestamp: 2, callerMSISDN: 3, callCellTowerId: 4, recieverMSISDN: 5, recieverTowerId: 6 }, { id: 2, timestamp: 3, callerMSISDN: 4, callCellTowerId: 5, recieverMSISDN: 6, recieverTowerId: 7 }],
-                vehicle: { registrationId: 1, registrationDate: "day", vehicleRegistrationNo: 2, make: "yes", model: "true", colour: "blue", forenames: "forename", surname: "surname", address: "england", dataOfBirth: "today", driverLicenceId: 3 },
-                associates: [{ citizenId: 1, forenames: "monika", surname: "mistry" }, { citizenId: 2, forenames: "owen", surname: "miller" }, { citizenId: 3, forenames: "rich", surname: "thi" }],
-                associates2: [{ forenames: "monika" }, { forenames: "owen" }, { forenames: "rich" }]
+                callRecords: [{ id: "", timestamp: "", callerMSISDN: "", callCellTowerId: "", recieverMSISDN: "", recieverTowerId: "" }, { id: "", timestamp: "", callerMSISDN: "", callCellTowerId: "", recieverMSISDN: "", recieverTowerId: "" }],
+                vehicle: { registrationId: "", registrationDate: "", vehicleRegistrationNo: "", make: "", model: "", colour: "", forenames: "", surname: "", address: "", dataOfBirth: "", driverLicenceId: "" },
+                associates: [{ citizenId: "", forenames: "", surname: "" }],
+                associates2: [{ forenames: "" }]
             });
         }).catch(response => {
             console.log(response)
