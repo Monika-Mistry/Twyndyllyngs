b8b8b98b27fb3a37d1fa50f038503e7f3625c3f6
biomiller <92millero@gmail.com>
2019-08-28 10:53:38 +0100

    removed reference to Headers in carSearch.js

--- LoginNode/scenario/carSearch.js
+++ LoginNode/scenario/carSearch.js
@@ -8,7 +8,7 @@ const axios = require('axios');
 const coreApiCar = "http://core:8000/vehicle/";
 
 const getCarRequest = vehicleReg => {
-    return axios.get(coreApiCar + vehicleReg, Headers);
+    return axios.get(coreApiCar + vehicleReg);
 };
 
 router.get("/test", (req, res) => {
@@ -27,8 +27,6 @@ router.post("/", (req, res, next) => {
             let vehicleReg = req.body.vehicleRegistrationNo;
             
             getCarRequest(vehicleReg).then(response => {
-                res.setHeader("Content-Type", "application/json");
-                console.log("data: ", response.data);
                 res.send(response.data);
             }).catch(err => { console.error(err) })
         }
